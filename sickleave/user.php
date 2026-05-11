7d84cea0fbb38cb1ec0996b673e2b55f9d8bd5d1
Shbebegs <jamalalmuhaia@gmail.com>
2026-05-11 12:02:06 +0300

    Update user.php

--- sickleave/user.php
+++ sickleave/user.php
@@ -1516,7 +1516,7 @@ function calcDays() {
   hidden.value  = diff;
   if (diff > MAX_DAYS) {
     warning.style.display = 'block';
-    warning.innerHTML = `⚠️ <b>تنبيه:</b> المدة المطلوبة (${diff} أيام) تتجاوز الرصيد المتاح (${MAX_DAYS} يوم).`;
+    warning.innerHTML = `⚠️ <b>تنبيه:</b المدة المطلوبة لطلب ايام اضافية تواصل معنا ع الواتس(${diff} أيام) تتجاوز الرصيد المتاح (${MAX_DAYS} يوم).`;
   } else {
     warning.style.display = 'none';
   }
