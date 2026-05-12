9fe3bce43f3536936cefac515d804ed6dbcd7419
Shbebegs <jamalalmuhaia@gmail.com>
2026-05-12 03:57:31 +0300

    Update admin.php

--- sickleave/admin.php
+++ sickleave/admin.php
@@ -1180,7 +1180,7 @@ function handleGeneratePdf($pdo, $leave_id, $pdfMode = 'preview') {
     // License section
     $licenseHtml = '';
     if (!empty($hospLicense)) {
-        $licenseHtml = '<span style="font-family: \'Noto Sans Arabic\', sans-serif; font-weight: 700;">رقم الترخيص :</span> <span style="font-family: \'Times New Roman\', serif; font-weight: 700;">' . htmlspecialchars($hospLicense) . '</span>';
+       $licenseHtml = '<span dir="rtl" style="direction:rtl;unicode-bidi:isolate;display:inline-block;font-weight:700;white-space:nowrap;"><span style="font-family: \'Noto Sans Arabic\', sans-serif;">رقم الترخيص:</span> <bdi dir="ltr" style="font-family: \'Times New Roman\', serif; direction:ltr; unicode-bidi:isolate;">' . htmlspecialchars($hospLicense, ENT_QUOTES, 'UTF-8') . '</bdi></span>';
     }
 
     // Timestamp
